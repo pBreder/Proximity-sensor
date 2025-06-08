70f3dcad4945f26cb6c6b66f9da5931cc284cbdb
Pedro Breder Costa <pbrederc@gmail.com>
2025-06-08 01:21:39 -0300

    first commit

---
+++ Proximity_Sensor.ino
@@ -0,0 +1,62 @@
+int trigPin = 12;
+int echoPin = 13;
+int leds[] = {3, 5, 6, 9, 10};
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  for (int i = 0; i < 5; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+  digitalWrite(trigPin, LOW);
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = 0.034 * duration / 2;
+
+  Serial.print("Distância: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(leds[i], LOW);
+  }
+
+  if (distance > 5 && distance < 10) {
+    digitalWrite(leds[0], HIGH);
+  }
+  else if (distance >= 10 && distance < 15) {
+    digitalWrite(leds[0], HIGH);
+    digitalWrite(leds[1], HIGH);
+  }
+  else if (distance >= 15 && distance < 20) {
+    digitalWrite(leds[0], HIGH);
+    digitalWrite(leds[1], HIGH);
+    digitalWrite(leds[2], HIGH);
+  }
+  else if (distance >= 20 && distance < 25) {
+    digitalWrite(leds[0], HIGH);
+    digitalWrite(leds[1], HIGH);
+    digitalWrite(leds[2], HIGH);
+    digitalWrite(leds[3], HIGH);
+  }
+  else if (distance >= 25) {
+    digitalWrite(leds[0], HIGH);
+    digitalWrite(leds[1], HIGH);
+    digitalWrite(leds[2], HIGH);
+    digitalWrite(leds[3], HIGH);
+    digitalWrite(leds[4], HIGH);
+  }
+
+  delay(300);
+}
